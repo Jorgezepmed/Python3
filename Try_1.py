15b7318cb968df5fa0aa33bcba3b733669e35cfd
Jorgezepmed <jorge.david.zepmed@gmail.com>
2019-12-17 01:19:42 -0600

    new test

---
+++ Try_1.py
@@ -0,0 +1,12 @@
+while True:
+  try:
+    n = float(input("Introduce un numero:"))
+    print(n + 5)
+
+  except:
+    print("Ocurrio un error intentalo de nuevo")
+
+  else:
+    print("Todo funcioono correcto")
+
+    break
